5c13f0333137fcdb524c4e065c8a5d703a58e21b
nrposner <nicolasposner@gmail.com>
2025-05-29 15:00:07 -0500

    added dummy test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
-# # Run the tests with: pytest tests/test_app.py
-# from ..app import add_numbers
-#
-# def test_add_numbers():
-#     assert 3 == add_numbers(1,2)
+# Run the tests with: pytest tests/test_app.py
+def add_numbers(x: int, y: int):
+    return x + y
+
+def test_add_numbers():
+    assert 3 == add_numbers(1,2)
